2372764299beaf911c2d374e0b370819a0c61883
Nahuel <nahuelguerra56b@gmail.com>
2023-05-10 20:25:52 -0300

    menu-hamburgesa-fin

--- app.ts
+++
@@ -1,9 +0,0 @@
-/* Cuando hago CLICJ .button. .nav TOGGLE 'activo' */
-
-
-const button = (document.querySelector('.button-burger') as HTMLButtonElement)
-const nav    = (document.querySelector('.nav')    as HTMLElement)
-
-button.addEventListener('click',() : void => {
-    nav.classList.toggle('activo')
-})
