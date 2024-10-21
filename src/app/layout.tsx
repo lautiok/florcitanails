fe916affd6dc83846db3f10456965f863f88e2c3
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-10-21 14:48:50 -0300

    fix:SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,29 @@ export default function RootLayout({
   metadata.title =
     "Florcita Nails - Servicios de manicura y academia en Buenos Aires";
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Florcita Nails no es solo un salón de belleza, ¡es un estudio de uñas y una academia!"
+        />
+        <meta
+          property="og:title"
+          content="Florcita Nails - Servicios de manicura y academia en Buenos Aires"
+        />
+        <meta
+          property="og:description"
+          content="Florcita Nails no es solo un salón de belleza, ¡es un estudio de uñas y una academia!"
+        />
+        <meta
+          property="og:image"
+          content="https://www.florcitanails.com.ar/logo.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="og:url" content="https://www.florcitanails.com.ar/" />
+        <link rel="canonical" href="https://florcitanails.com.ar/" />
+      </head>
       <body className={`${poppins && poppins.variable}`}>
         <Header />
         {children}
