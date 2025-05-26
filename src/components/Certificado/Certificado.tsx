ddb8a28c60440848025aa28fa18623f34904c350
“Nahuel <nahuelguerra56b@gmail.com>
2025-05-26 13:01:17 -0300

    remplace dom-to-image

--- src/components/Certificado/Certificado.tsx
+++ src/components/Certificado/Certificado.tsx
@@ -1,73 +1,97 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useRef } from "react";
-import html2canvas from "html2canvas";
 import styles from "./certificado.module.css";
 import { useSession } from "next-auth/react";
+import domtoimage from "dom-to-image";
 
 interface CertificadoProps {
   nombre: string;
   curso: string;
   fecha: string;
-  id : string;
+  id: string;
 }
 
-const Certificado: React.FC<CertificadoProps> = ({ nombre, curso, fecha, id }) => {
+const Certificado: React.FC<CertificadoProps> = ({
+  nombre,
+  curso,
+  fecha,
+  id,
+}) => {
   const certificadoRef = useRef<HTMLDivElement>(null);
   const { data: session } = useSession();
 
-
   const handleDownload = async () => {
-    if (certificadoRef.current) {
-      const canvas = await html2canvas(certificadoRef.current, { scale: 2 });
-      const imageData = canvas.toDataURL("image/png");
-      const link = document.createElement("a");
-      link.href = imageData;
-      link.download = `Certificado_${nombre}.png`;
-      link.click();
-    }
-  };
+    const node = certificadoRef.current!;
+    const scale = 5;
 
-  const DelateCertificado = async () => {
+    const style = {
+      transform: "scale(" + scale + ")",
+      transformOrigin: "top left",
+      width: node.offsetWidth + "px",
+      height: node.offsetHeight + "px",
+    };
+
+    const param = {
+      height: node.offsetHeight * scale,
+      width: node.offsetWidth * scale,
+      style,
+    };
+
+    const dataUrl = await domtoimage.toPng(node, param);
+
+    const link = document.createElement("a");
+    link.href = dataUrl;
+    link.download = `Certificado_${nombre}.png`;
+    link.click();
+  };
+  const deleteCertificado = async () => {
     if (session?.user?.role === "admin") {
       const response = await fetch(`/api/certificate/${id}`, {
         method: "DELETE",
       });
       if (response.ok) {
-        location.href = "/dashboard/certificados";
-
+        window.location.href = "/dashboard/certificados";
       } else {
         alert("Error al eliminar el certificado");
       }
     }
-  }
+  };
 
   return (
-    <div key="certificado" className={styles.container}>
+    <div className={styles.container}>
       <div ref={certificadoRef} className={styles.certificado}>
-        <img src="/logo.png" alt="Logo" className={styles.logo} />
-        <h1 className={styles.title}>CERTIFICADO</h1>
-        <p className={styles.subtitle}>Certifico que</p>
-        <h2 className={styles.name}>{nombre}</h2>
-        <p className={styles.description}>
-          Ha realizado y completado con éxito su curso de uñas{" "}
-          <strong>{curso}</strong>.
-        </p>
-        <p className={styles.date}>{fecha}</p>
-        <div className={styles.signatureSection}>
-          <p>Florencia Valles</p>
-          <small>Manicurista y fundadora de Florcitanails</small>
+        <img
+          src="/certificado.jpg"
+          alt="Fondo"
+          className={styles.bgImage}
+          crossOrigin="anonymous"
+        />
+        <div className={styles.content}>
+          <img src="/logo.png" alt="Logo" className={styles.logo} />
+          <h1 className={styles.title}>CERTIFICADO</h1>
+          <p className={styles.subtitle}>Certifico que</p>
+          <h2 className={styles.name}>{nombre}</h2>
+          <p className={styles.description}>
+            Ha realizado y completado con éxito su curso de uñas{" "}
+            <strong>{curso}</strong>.
+          </p>
+          <p className={styles.date}>{fecha}</p>
+          <div className={styles.signatureSection}>
+            <p>Florencia Valles</p>
+            <small>Manicurista y fundadora de Florcitanails</small>
+          </div>
         </div>
       </div>
+
       <button onClick={handleDownload} className={styles.downloadButton}>
         Descargar Certificado
       </button>
 
       {session?.user?.role === "admin" && (
-        <button onClick={DelateCertificado} className={styles.deleteButton}>
+        <button onClick={deleteCertificado} className={styles.deleteButton}>
           Eliminar Certificado
         </button>
       )}
-
     </div>
   );
 };
